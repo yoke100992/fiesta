141b74d287bbcf9737ff5564ae8c12fc4d77ed4f
yoke100992 <138417386+yoke100992@users.noreply.github.com>
2026-08-05 20:47:19 +0700

    Update Permit.jsx

--- src/pages/Permit.jsx
+++ src/pages/Permit.jsx
@@ -17,7 +17,7 @@ export default function Permit() {
   const [permitData, setPermitData] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  const statusOptions = ["SAKIT", "IZIN", "CUTI", "OFF", "MEETING", " TIDAK ADA PENJUALAN"];
+  const statusOptions = ["SAKIT", "IZIN", "CUTI", "OFF", "MEETING"];
 
   // 🔥 LOAD AWAL
   useEffect(() => {
